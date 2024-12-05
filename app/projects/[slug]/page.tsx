590ce161aef51ba1378c0e792c25309d290b5f51
Mathiew Abbas <mathiew95@gmail.com>
2024-12-04 21:27:25 -0600

    feat: removed redis and added pocketbase

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,47 +1,26 @@
-import { notFound } from "next/navigation";
-import { allProjects } from "contentlayer/generated";
-import { Mdx } from "@/app/components/mdx";
-import { Header } from "./header";
-import "./mdx.css";
-import { ReportView } from "./view";
-import { Redis } from "@upstash/redis";
+import { getProjectById } from '@/lib/pocketbase'
+import { notFound } from 'next/navigation'
+import { Header } from './header'
+import { ReportView } from './view'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
-export const revalidate = 60;
+export const revalidate = 10;
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-
-const redis = Redis.fromEnv();
-
-export async function generateStaticParams(): Promise<Props["params"][]> {
-  return allProjects
-    .filter((p) => p.published)
-    .map((p) => ({
-      slug: p.slug,
-    }));
-}
-
-export default async function PostPage({ params }: Props) {
+export default async function PostPage({ params }: { params: { slug: string } }) {
   const slug = params?.slug;
-  const project = allProjects.find((project) => project.slug === slug);
+  const project = await getProjectById(slug)
 
   if (!project) {
-    notFound();
+    notFound()
   }
-
-  const views =
-    (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} views={views} />
-      <ReportView slug={project.slug} />
+      <Header project={project} />
+      <ReportView id={project.id} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
-        <Mdx code={project.body.code} />
+        <MDXRemote source={project.content} />
       </article>
     </div>
   );
