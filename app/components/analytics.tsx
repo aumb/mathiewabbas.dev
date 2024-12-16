394dd7bb16f052b6bf8dc8e2592cb9926b19a9a8
Mathiew Abbas <mathiew95@gmail.com>
2024-12-15 21:02:05 -0600

    feat: added analytics

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	if (!token) {
-		return null;
-	}
 	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
+		<script defer data-domain={process.env.DATA_DOMAIN} src={process.env.DATA_DOMAIN_SRC}></script>
 	);
 }
