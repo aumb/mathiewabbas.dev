e883a7b96eb5cf7a061931b6bc450c98aa459c0d
Mathiew Abbas <mathiew95@gmail.com>
2025-10-06 18:12:41 -0500

    feat: sped up animations

--- app/page.tsx
+++ app/page.tsx
@@ -68,8 +68,8 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 
-        className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-zinc-200 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text"
+      <h1
+        className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-500 bg-zinc-200 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text"
       >
         aumb
       </h1>
